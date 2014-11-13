4a1d017578c78dfa1ad295768999731b3121e598
Marcos Candeia <marrcooos@gmail.com>
2014-11-13 18:54:57 -0300

    Primeiro commit usando intellij

---
+++ src/br/com/les/where2go/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.les.where2go;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
