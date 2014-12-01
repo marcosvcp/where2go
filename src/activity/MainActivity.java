b1b37d1d0b3baa8e0bd714e16e639576ccb8eacf
Bruno <bruno.brandao@embedded.ufcg.edu.br>
2014-12-01 15:33:36 -0300

    [S01T20] Adicionando logica de acesso a tela de eventos

--- src/activity/MainActivity.java
+++ src/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package activity;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -7,11 +8,17 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import br.com.les.where2go.R;
+
 import com.facebook.AppEventsLogger;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Application core
@@ -19,13 +26,18 @@ import java.security.NoSuchAlgorithmException;
 public class MainActivity extends FragmentActivity {
 
     private MainFragment mainFragment;
-
+    private List<View> mViews;
+    
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ImageButton mBtnEventList = (ImageButton) findViewById(R.id.enterEventListButton);
+        mViews = new LinkedList<View>();
+        mViews.add(mBtnEventList);
+        
         if (savedInstanceState == null) {
             // Add the fragment on initial activity setup
             mainFragment = new MainFragment();
@@ -39,6 +51,15 @@ public class MainActivity extends FragmentActivity {
                     .findFragmentById(android.R.id.content);
         }
         setContentView(R.layout.main);
+        
+        mBtnEventList.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				lockAll();
+				enterEventList();
+			}
+		});
     }
 
     @Override
@@ -53,5 +74,31 @@ public class MainActivity extends FragmentActivity {
         super.onPause();
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
+    }
+    
+    /**
+     * Bloqueia todas as Views, para evitar que duas das mesmas sejam selecionadas ao mesmo tempo.
+     */
+    private void lockAll() {
+        for (final View v : mViews) {
+            v.setEnabled(false);
+        }
+    }
+    
+    /**
+     * Desbloqueia as Views.
+     */
+    private void unlockAll() {
+        for (final View v : mViews) {
+            v.setEnabled(true);
+        }
+    }
+    
+    /**
+     * Evento referente a ação do botão EnterEventList.
+     */
+    public void enterEventList() {
+    	final Intent intent = new Intent(MainActivity.this,EventListActivity.class);
+    	startActivity(intent);
     }
 }
