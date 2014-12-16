290839a15a94d183dce3c5b15067234d60dc3e4e
Júlio Andherson <julio.silva@embedded.ufcg.edu.br>
2014-12-15 22:03:45 -0300

    Complementando javadoc para pacote

--- src/Where2Go/src/utils/FieldValidation.java
+++ src/Where2Go/src/utils/FieldValidation.java
@@ -1,35 +1,52 @@
-
 package utils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 import android.content.Context;
 import android.widget.EditText;
 import br.com.les.where2go.R;
 
-
+/**
+ * The Class FieldValidation.
+ */
 public class FieldValidation {
     // Context
+    /** The context. */
     private final Context context;
     // Regular Expression
+    /** The email regex. */
     private final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    
+    /** The name regex. */
     private final String NAME_REGEX = "^[\\sa-zA-Z0-9_-]{3,16}$";
+    
+    /** The password regex. */
     private final String PASSWORD_REGEX = "^[a-zA-Z0-9_-]{6,16}$";
+    
+    /** The num ap regex. */
     private final String NUM_AP_REGEX = "^[\\sa-zA-Z0-9_-]{1,10}$";
+    
+    /** The currency regex. */
     private final String CURRENCY_REGEX = "(0|[1-9]+[0-9]*)?((\\.[0-9]{2})|(\\[.|,][0-9]{2}))?";
 
-    //private final String CURRENCY_REGEX = "(0|[1-9]+[0-9]*)?(\\.[0-9]{2})?";
-
-    // private final String PHONE_REGEX =
-    // "^(?:(?([0-9]{2}))?[-. ]?)?([0-9]{4})[-. ]?([0-9]{4})$";
+    /**
+     * Instantiates a new field validation.
+     *
+     * @param c the c
+     */
     public FieldValidation(Context c) {
         context = c;
     }
 
-    // return true if the input field is valid, based on the parameter
-    // passed
+    /**
+     * Checks if is valid.
+     *
+     * @param editText the edit text
+     * @param regex the regex
+     * @param errMsg the err msg
+     * @param required the required
+     * @return true, if is valid
+     */
     public boolean isValid(EditText editText, String regex, String errMsg, boolean required) {
         String text = editText.getText().toString().trim();
         // clearing the error, if it was previously set by some other
@@ -47,8 +64,12 @@ public class FieldValidation {
         return true;
     }
 
-    // check the input field has any text or not
-    // return true if it contains text otherwise false
+    /**
+     * Checks for text.
+     *
+     * @param editText the edit text
+     * @return true, if successful
+     */
     public boolean hasText(EditText editText) {
         String text = editText.getText().toString().trim();
         editText.setError(null);
@@ -59,5 +80,4 @@ public class FieldValidation {
         }
         return true;
     }
-
 }
