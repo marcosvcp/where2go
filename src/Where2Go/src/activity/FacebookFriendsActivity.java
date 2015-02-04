d133284df2af6cdc938455302e760f5e21b80bfb
Júlio Andherson <julio.silva@embedded.ufcg.edu.br>
2015-02-03 21:52:52 -0300

    Botão de convidar na actionbar

--- src/Where2Go/src/activity/FacebookFriendsActivity.java
+++ src/Where2Go/src/activity/FacebookFriendsActivity.java
@@ -18,8 +18,12 @@ import adapter.FacebookFriendsAdapter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class FacebookFriendsActivity extends Activity {
 
@@ -69,5 +73,25 @@ public class FacebookFriendsActivity extends Activity {
 						}
 					}
 				}).executeAsync();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.invite_friends_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_done:
+	            Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_LONG).show();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 }
