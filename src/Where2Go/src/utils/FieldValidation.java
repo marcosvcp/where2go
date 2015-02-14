d6a8a1e76670aba0c81654d9c2efbab23bd1610d
Júlio Andherson <julio.silva@embedded.ufcg.edu.br>
2015-02-14 15:39:51 -0300

    Checkstyle concluido no pacote utils

--- src/Where2Go/src/utils/FieldValidation.java
+++ src/Where2Go/src/utils/FieldValidation.java
@@ -1,10 +1,12 @@
-package utils;
 
-import java.util.regex.Pattern;
+package utils;
 
 import android.content.Context;
 import android.widget.EditText;
+
 import br.com.les.where2go.R;
+
+import java.util.regex.Pattern;
 
 /**
  * The Class FieldValidation.
@@ -13,47 +15,29 @@ public class FieldValidation {
     // Context
     /** The context. */
     private final Context context;
-    // Regular Expression
-    /** The email regex. */
-    private final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-    /** The name regex. */
-    private final String NAME_REGEX = "^[\\sa-zA-Z0-9_-]{3,16}$";
-
-    /** The password regex. */
-    private final String PASSWORD_REGEX = "^[a-zA-Z0-9_-]{6,16}$";
-
-    /** The num ap regex. */
-    private final String NUM_AP_REGEX = "^[\\sa-zA-Z0-9_-]{1,10}$";
 
     /** The currency regex. */
-    private final String CURRENCY_REGEX = "(0|[1-9]+[0-9]*)?((\\.[0-9]{2})|(\\[.|,][0-9]{2}))?";
 
     /**
      * Instantiates a new field validation.
-     *
-     * @param c
-     *            the c
+     * 
+     * @param c the c
      */
-    public FieldValidation(Context c) {
+    public FieldValidation(final Context c) {
         context = c;
     }
 
     /**
      * Checks if is valid.
-     *
-     * @param editText
-     *            the edit text
-     * @param regex
-     *            the regex
-     * @param errMsg
-     *            the err msg
-     * @param required
-     *            the required
+     * 
+     * @param editText the edit text
+     * @param regex the regex
+     * @param errMsg the err msg
+     * @param required the required
      * @return true, if is valid
      */
-    public boolean isValid(EditText editText, String regex, String errMsg,
-            boolean required) {
+    public final boolean isValid(final EditText editText, final String regex, final String errMsg,
+            final boolean required) {
         String text = editText.getText().toString().trim();
         // clearing the error, if it was previously set by some other
         // values
@@ -72,12 +56,11 @@ public class FieldValidation {
 
     /**
      * Checks for text.
-     *
-     * @param editText
-     *            the edit text
+     * 
+     * @param editText the edit text
      * @return true, if successful
      */
-    public boolean hasText(EditText editText) {
+    public final boolean hasText(final EditText editText) {
         String text = editText.getText().toString().trim();
         editText.setError(null);
         // length 0 means there is no text
