b3b0dfa4e5acf209aaaff5815fe9bf01893261d4
diegotavarez <diegotavarez@github.com>
2015-02-15 13:25:25 -0200

    Opção de invite na tela de detalhe e carregamento dos dados do evento

--- src/Where2Go/src/activity/EventDetailActivity.java
+++ src/Where2Go/src/activity/EventDetailActivity.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import persistence.ParseUtil;
 import android.R.color;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -35,11 +39,43 @@ public class EventDetailActivity extends FragmentActivity {
     private Event event;
 
     /** The event name. */
-    private TextView etEventName;
+    private TextView tvEventName;
     
     /** The event desscription. */
-    private TextView etEventDescription;
+    private TextView tvEventDescription;
 
+    /** The event initialDate. */
+    private TextView tvInitialDate;
+    
+    /** The event final date. */
+    private TextView tvFinalDate;
+    
+    /** The event initial hour. */
+    private TextView tvInitialHour;
+    
+    /** The event final hour. */
+    private TextView tvFinalHour;
+    
+    /** The event capacity. */
+    private TextView tvCapacity;
+    
+    /** The event status. */
+    private TextView tvStatus;
+    
+    /** The event price. */
+    private TextView tvPrice;
+    
+    /** The event outfit. */
+    private TextView tvOutfit;
+    
+    /** The event owner. */
+    private TextView tvOwner;
+    
+    /** The event creation date. */
+    private TextView tvCreatedAt;
+    
+    /** The event notes. */
+    private TextView tvNotes;
     
     /**
      * Called when the activity is first created.
@@ -50,13 +86,25 @@ public class EventDetailActivity extends FragmentActivity {
     public final void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
-        setStatusBarColor(findViewById(R.id.statusBarBackground),color.transparent);
-        
+        setStatusBarColor(findViewById(R.id.statusBarBackground),
+                getResources().getColor(R.color.status_bar));        
         final Bundle data = getIntent().getExtras();
         final String key = data.getString("event_id");
         
-        etEventName = (TextView) findViewById(R.id.et_event_name_detail);
-        etEventDescription = (TextView) findViewById(R.id.et_event_description_detail);
+        tvEventName = (TextView) findViewById(R.id.tv_event_name_detail);
+        tvEventDescription = (TextView) findViewById(R.id.tv_event_description_detail);
+        tvInitialDate = (TextView) findViewById(R.id.tv_data_inicio);
+        tvFinalDate = (TextView) findViewById(R.id.tv_detail_final_date);
+        tvInitialHour = (TextView) findViewById(R.id.tv_hora_inicio);
+        tvFinalHour = (TextView) findViewById(R.id.tv_detail_final_hour);
+        
+        tvCapacity = (TextView) findViewById(R.id.tv_detail_capacity);
+        tvStatus = (TextView) findViewById(R.id.tv_detail_status);
+        tvPrice = (TextView) findViewById(R.id.tv_detail_price);
+        tvOutfit = (TextView) findViewById(R.id.tv_detail_outfit);
+        tvOwner = (TextView) findViewById(R.id.tv_detail_owner);
+        tvCreatedAt = (TextView) findViewById(R.id.tv_detail_created_at);
+        tvNotes = (TextView) findViewById(R.id.tv_detail_notes);
         
         // Busca no servidor o Objeto que tem o ID
         ParseUtil.findEventById(key, new GetCallback<Event>() {
@@ -74,15 +122,49 @@ public class EventDetailActivity extends FragmentActivity {
      * Set all fields of view with name of events fields.
      */
     private void setDataFields() {
-        etEventName.setText(event.getName());
-        etEventDescription.setText(event.getDescription());
-//        etEventInitialDate.setText(ParseUtil.ptbr.format(event
-//                .getInitialDate()));
-//        etEventFinalDate
-//                .setText(ParseUtil.ptbr.format(event.getFinalDate()));
-//        etEventInitialTime.setText(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + ":"
-//                + Calendar.getInstance().get(Calendar.MINUTE));
-//        etEventFinalTime.setText("23:59");
+        tvEventName.setText(event.getName());
+        tvEventDescription.setText(event.getDescription());
+        tvInitialDate.setText(ParseUtil.ptbr.format(event.getInitialDate()));
+        tvFinalDate.setText(ParseUtil.ptbr.format(event.getFinalDate()));
+        tvCapacity.setText(String.valueOf(event.getCapacity()));
+        tvStatus.setText(event.getState());
+        tvPrice.setText(String.valueOf(event.getPrice()));
+        tvOutfit.setText(event.getOutfit());
+        tvNotes.setText(event.getNote());
+        tvOwner.setText(event.getOwnerName());
+        tvCreatedAt.setText(ParseUtil.ptbr.format(event.getCreatedAt()));
+    }
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.event_detail_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public final boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_invite:
+                final Intent intent = new Intent(
+                        getApplicationContext(),
+                        FacebookFriendsActivity.class);
+                intent.putExtra("EventId", event.getObjectId());
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /**
