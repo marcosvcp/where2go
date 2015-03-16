2b796111beb98651897b07bcb9467d99b6f94562
marcosvcp <marrcooos@gmail.com>
2015-03-16 15:08:53 -0300

    Adicionando zoom no mapa e marcando a localizacao do usuario

--- src/Where2Go/src/activity/MapActivity.java
+++ src/Where2Go/src/activity/MapActivity.java
@@ -1,13 +1,25 @@
 package activity;
 
 import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import br.com.les.where2go.R;
 
 /**
  * The Class MapFragment.
  */
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements OnMapReadyCallback {
 
     /**
      * Instantiates a new map fragment.
@@ -25,5 +37,22 @@ public class MapActivity extends Activity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = service.getBestProvider(criteria, false);
+        Location location = service.getLastKnownLocation(provider);
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13));
+        map.addMarker(new MarkerOptions()
+                .position(userLocation)
+                .title("Flesta em Casa"));
     }
 }
